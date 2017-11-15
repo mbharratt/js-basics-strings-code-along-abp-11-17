362c13d4915bf0529682a10d59c3c8cd4c03ba35
Melissa Bharratt <melissa.bharratt@gmail.com>
2017-11-15 23:14:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser .slice (0, 1)}!`;
+
+'Edsger Dijkstra'[0];
+//=> "E"
+
+'Edsger Dijkstra' .charAt(0);
+
+'Edsger Dijkstra' .length;
+//=>15
+
+'Edsger Dijkstra' [15];
+// => undefined
+
+'Edsger Dijkstra' [14];
+//=> "a"
+
+'Edsger Dijkstra' .slice();
+//=> "Edsger Dijkstra"
+
+'Edsger Dijkstra' .slice(7);
+//=> "Dijkstra"
+
+'Edsger Dijkstra' .slice(0, 3);
+//=> "Eds"
+
+currentUser.slice(0, 1);
